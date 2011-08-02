4adbb5307ef1a863fd5a34e6f1e75fe8ea081557
Matthew Williams <matthew@flowroute.com>
2011-08-02 11:35:40 -0500

    ignore build & dist; use sys.path[0] in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+import sys
 
 def listify(filename):
     return filter(None, open(filename, 'r').readlines())
@@ -9,11 +10,11 @@ setup(
     url = 'http://github.com/dmaclay/python-smpp',
     license = 'BSD',
     description = "Python SMPP Library",
-    long_description = open('README.rst','r').read(),
+    long_description = open(sys.path[0] + '/README.rst','r').read(),
     author = 'David Maclay',
     author_email = 'dev@praekeltfoundation.org',
     packages = find_packages(),
-    install_requires = ['setuptools'].extend(listify('requirements.pip')),
+    install_requires = ['setuptools'].extend(listify(sys.path[0] + '/requirements.pip')),
     classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
